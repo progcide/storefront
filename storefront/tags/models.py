6964914f31fc4903c91f546869f04fe65eb4b88f
progcide <progcide@gmail.com>
2025-11-27 23:44:25 +0500

    add Classes in tags app and add new app called likes

--- storefront/tags/models.py
+++ storefront/tags/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE
+    )
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
